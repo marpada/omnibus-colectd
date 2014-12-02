c52a7c69ed9f29ce9435c4724d9cdf0543330387
dpando <david.pando@localist.co.nz>
2014-12-02 15:55:14 +1300

    Config file for libxml2

---
+++ config/software/libxml2.rb
@@ -0,0 +1,25 @@
+name "libxml2"
+default_version "2.9.2"
+
+
+source :url => "ftp://xmlsoft.org/libxml2/libxml2-#{version}.tar.gz",
+        :md5 => "9e6a9aca9d155737868b3dc5fd82f788" # 2.9.2
+
+relative_path "libxml2-#{version}"
+
+
+env = {
+  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+  "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
+}
+
+build do
+  command ["./configure",
+           "--prefix=#{install_dir}/embedded",
+          "--without-python",
+  ], :env => env
+  command "make"
+  command "make install"
+
+end
